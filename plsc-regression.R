35bffc51d0996b0bf2326a5188a053b2f8524b5e
chlohamp <champ007@fiu.edu>
2025-05-06 12:33:39 -0400

    Add regression analysis to PLSC scripts

--- plsc-regression.R
+++ plsc-regression.R
@@ -1,22 +1,33 @@
 library("lmerTest")
 library("readr")
 
-data_dir <- "/Users/chloehampson/Desktop/projects/abide-analysis/dset/group/habenula/pheno/" # Make sure to leave the slash at the end
-clusters <- c("1", "1a", "2", "2a")
-roi <- "RSFC"
+data_dir <- "/Users/chloehampson/Desktop/projects/abcd-plsc/derivatives/regression/" # Make sure to leave the slash at the end
+networks <- c("cgc-dt", "dt-dla", "dt-dt")
+roi <- "rsfc"
 
 # Level-1 Predictors
-group_var <- "Group"
-categorical_vars <- c("Sex")
-numerical_vars <- c("Age")
-phen_vars <- c("Phen1", "Phen2", "Phen3", "Phen4")
+categorical_vars <- c("demo_sex_v2", "demo_prnt_gender_id_v2", "demo_origin_v2", "mri_info_manufacturer")
+numerical_vars <- c("interview_age", "demo_prnt_age_v2", "demo_prnt_ed_v2_2yr_l", "demo_prtnr_ed_v2_2yr_l", "demo_comb_income_v2", "rsfmri_meanmotion")
+phyhealth_vars <- c(
+    "mctq_sdweek_calc",
+    "mctq_msfsc_calc",
+    "resp_wheeze_yn_y",
+    "resp_pmcough_yn_y",
+    "resp_diagnosis_yn_y",
+    "resp_bronch_yn_y",
+    "blood_pressure_sys_mean",
+    "blood_pressure_dia_mean",
+    "physical_activity1_y",
+    "cbcl_scr_syn_internal_t",
+    "cbcl_scr_syn_external_t"
+)
 
-for (cluster in clusters) {
-    data_path <- paste0(data_dir, "cluster-", cluster, "_data.csv")
+for (network in networks) {
+    data_path <- paste0(data_dir, "phyhealth_", network, "_data.csv")
     data <- read.table(file = data_path, sep = ",", header = TRUE)
 
-    for (phen_var in phen_vars) {
-        all_columns <- c(roi, categorical_vars, numerical_vars, phen_var, group_var, "Site")
+    for (phyhealth_var in phyhealth_vars) {
+        all_columns <- c(roi, categorical_vars, numerical_vars, phyhealth_var, "site_id_l")
         sub_data <- data[, all_columns]
         sub_data <- na.omit(sub_data)
 
@@ -25,21 +36,18 @@ for (cluster in clusters) {
             sub_data[[var]] <- factor(sub_data[[var]])
         }
 
-        # Relevel 'Group' to make NT the reference category
-        sub_data[[group_var]] <- relevel(factor(sub_data[[group_var]]), ref = "NT")
-
         # Scale continuous predictors
         for (var in numerical_vars) {
             sub_data[[var]] <- scale(sub_data[[var]], center = TRUE, scale = TRUE)
         }
 
-        sub_data[[phen_var]] <- scale(sub_data[[phen_var]], center = TRUE, scale = TRUE)
+        sub_data[[phyhealth_var]] <- scale(sub_data[[phyhealth_var]], center = TRUE, scale = TRUE)
 
         # Fixed effects formula
         fixed_effects <- paste(c(numerical_vars, categorical_vars), collapse = " + ")
 
-        # Full model with interaction
-        equation_lme <- paste(roi, "~", fixed_effects, "+", group_var, "*", phen_var, "+ (1|Site)")
+        # Full model
+        equation_lme <- paste(roi, "~", fixed_effects, "+", phyhealth_var, "+ (1|site_id_l)")
 
         # Run the full model
         model <- lmer(as.formula(equation_lme), data = sub_data)
@@ -48,21 +56,7 @@ for (cluster in clusters) {
 
         # Save results
         model_table <- as.data.frame(coef(summary(model)))
-        out_file <- paste0(data_dir, "cluster-", cluster, "_", phen_var, "_table.csv")
+        out_file <- paste0(data_dir, "phyhealth_", network, "_", phyhealth_var, "_table.csv")
         write.csv(model_table, file = out_file, row.names = TRUE)
-
-        # ASD-Only Model (Main Effects Only, No Interaction)
-        asd_data <- sub_data[sub_data[[group_var]] == "ASD", ]
-        equation_lme_asd <- paste(roi, "~", paste(c(fixed_effects, phen_var), collapse = " + "), "+ (1|Site)")
-
-        # Run ASD model
-        model_asd <- lmer(as.formula(equation_lme_asd), data = asd_data)
-        model_asd_summary <- summary(model_asd)
-        print(model_asd_summary)
-
-        # Save ASD-only results
-        asd_phen_effect <- as.data.frame(coef(summary(model_asd)))
-        out_file_asd <- paste0(data_dir, "cluster-", cluster, "_", phen_var, "_ASD_only.csv")
-        write.csv(asd_phen_effect, file = out_file_asd, row.names = TRUE)
     }
 }
